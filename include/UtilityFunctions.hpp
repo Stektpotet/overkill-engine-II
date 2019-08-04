1bf06efe6fb1f06525ba2d29f7bf2c1492b6e34c
JonShard <jonems@stud.ntnu.no>
2019-08-04 12:24:23 +0200

    Added degrees-radians conversion consts.

--- include/UtilityFunctions.hpp
+++ include/UtilityFunctions.hpp
@@ -5,9 +5,12 @@
 
 namespace OK::Util
 {
+    const float PI = 3.14159;
+    const float Deg2Rad = (1.0f / 180.0f) * PI;
+    const float Rad2Deg = (1.0f / PI) * 180.0f;
+
     void processError(int code, const char* description);
     glm::uint packSnorm10x3_2Rev(glm::vec3 const& v);
     glm::uint packSnorm10x3_2Rev(glm::vec3 const& v, bool const& flag0, bool const& flag1);
     glm::uint16 packTexCoords(glm::vec2 const& v);
-
 }
